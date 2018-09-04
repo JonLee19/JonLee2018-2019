55d00e40c4edba1252163d08581578732fe47854
APCS3rd <APCS3rd@MHS-Dreyer-13>
2018-09-04 11:04:38 -0700

    Pikachu code 9/4

---
+++ Pikachu3rdperiod/src/Pikachu3.java
@@ -0,0 +1,13 @@
+//jon 8/31/18
+//creating pikachu
+public class Pikachu3 {
+
+	public static void main(String[] args) {
+		System.out.println("Pikachu welcomes you to the world of Pokemon!");
+		System.out.println("			 (\\__/)");
+		System.out.println("			 (o^.^)");
+		System.out.println("			z(_(\")(\")");
+
+	}
+
+}
